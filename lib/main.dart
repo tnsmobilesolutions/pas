de48a03674355e252963b46bab37535d005c3b1f
Amrutanshu <96327371+Amrutanshu-Chunu@users.noreply.github.com>
2022-10-14 15:40:59 +0530

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
-import 'package:prabasi_anchalika_sangha/screen/signup.dart';
-
-
+import 'package:prabasi_anchalika_sangha/screen/loginScreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,74 +11,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final appName = "Prabasi Anchalika Sangha";
+    const appName = "Prabasi Anchalika Sangha";
     // ignore: prefer_const_constructors
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: appName,
       theme: ThemeData(
-          brightness: Brightness.light,
+          // brightness: Brightness.light,
           primaryColor: Colors.deepPurple,
-          scaffoldBackgroundColor: Color.fromARGB(235, 187, 199, 211),
-          appBarTheme:
-              AppBarTheme(backgroundColor: Color.fromARGB(255, 44, 14, 154)),
+          scaffoldBackgroundColor: const Color(0xFFf6f6f6),
+          appBarTheme: AppBarTheme(backgroundColor: Color(0xFFf6f6f6)),
           fontFamily: 'Georgia',
           elevatedButtonTheme: ElevatedButtonThemeData(
               style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 91, 76, 175)),
+            backgroundColor:
+                MaterialStateProperty.all<Color>(Color(0xFFfa6e0f)),
           ))),
       home: LoginPage(
         title: appName,
-      ),
-    );
-  }
-}
-
-class LoginPage extends StatelessWidget {
-  final String title;
-
-  LoginPage({
-    Key? key,
-    required this.title,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    const inputDecoration =
-        const InputDecoration(icon: Icon(Icons.person), labelText: "User ID");
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            const SizedBox(height: 10),
-            TextFormField(
-              decoration: inputDecoration,
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                  icon: Icon(Icons.lock), labelText: "Password"),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Sign In'),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const SignUpWidget()),
-                );
-              },
-              child: const Text('Sign Up'),
-            )
-          ],
-        ),
       ),
     );
   }
---
+++ lib/screen/loginScreen.dart
@@ -0,0 +1,37 @@
+import 'package:authentication/EmailLogin/authenticationWidget.dart';
+import 'package:authentication/EmailLogin/sign_up.dart';
+import 'package:authentication/user_model.dart';
+import 'package:flutter/material.dart';
+import 'package:prabasi_anchalika_sangha/screen/homescreen.dart';
+
+class LoginPage extends StatelessWidget {
+  final String title;
+
+  LoginPage({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFf6f6f6),
+      body: SafeArea(
+        child: AuthenticationWidget(
+          scaffoldbackGroundColor: Color(0xFFf6f6f6),
+          isSignUpVisible: true,
+          phoneAuthentication: false,
+          onEmailLoginPressed: (useremail, userpassword) {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => HomeWidget(),
+                ));
+          },
+          isImageVisible: false,
+          onSignUpPressed: (userModel) {},
+        ),
+      ),
+    );
+  }
+}
